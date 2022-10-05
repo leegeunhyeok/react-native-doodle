b67979e9183a1f23af4759fdc2a5d4b3b389821e
leegeunhyeok <dev.ghlee@gmail.com>
2022-10-06 03:38:27 +0900

    chore: add assets
    - add hand drawn images (svg)
      - from https://github.com/chr15m/DoodleCSS

---
+++ src/assets/index.ts
@@ -0,0 +1,5 @@
+export { default as Border } from './border.svg';
+export { default as Button } from './button.svg';
+export { default as Caret } from './caret.svg';
+export { default as Checkbox } from './checkbox.svg';
+export { default as Hr } from './hr.svg';
