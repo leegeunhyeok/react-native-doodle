fb22bb9381c8f689cf8af0d1a267b0864ecab6b9
leegeunhyeok <dev.ghlee@gmail.com>
2022-10-12 03:33:05 +0900

    feat: add constants
    - add svg default props

---
+++ src/constants/index.ts
@@ -0,0 +1,13 @@
+export const SVG_CONTAINER_STYLE = {
+  position: 'absolute',
+  top: 0,
+  right: 0,
+  bottom: 0,
+  left: 0,
+} as const;
+
+export const DEFAULT_SVG_PROPS = {
+  width: '100%',
+  height: '100%',
+  preserveAspectRatio: 'none',
+} as const;
