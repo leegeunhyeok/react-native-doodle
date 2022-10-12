2ba7a0d46f8f3370da58ba28c8004977da3dd079
leegeunhyeok <dev.ghlee@gmail.com>
2022-10-13 03:49:00 +0900

    feat: add `inspired by` content

--- example/src/components/Description.tsx
+++ example/src/components/Description.tsx
@@ -5,6 +5,7 @@ import DoodleText from './DoodleText';
 
 const PACKAGE_NAME = 'react-native-doodle';
 const PROFILE_URL = 'https://github.com/leegeunhyeok';
+const DOODLE_CSS_URL = 'https://chr15m.github.io/DoodleCSS';
 
 const Description = () => {
   const handlePressProfile = () => {
@@ -15,11 +16,15 @@ const Description = () => {
     Linking.openURL(`${PROFILE_URL}/${PACKAGE_NAME}`);
   };
 
+  const handlePressInspired = () => {
+    Linking.openURL(DOODLE_CSS_URL);
+  };
+
   return (
     <View style={styles.section}>
       <DoodleText style={styles.h1}>react-native-doodle</DoodleText>
       <DoodleText style={styles.p}>
-        A simple hand drawn theme for React Native.
+        A simple hand drawn theme for React Native.{' '}
       </DoodleText>
       <DoodleText style={styles.p}>
         By{' '}
@@ -32,6 +37,9 @@ const Description = () => {
         <DoodleText style={styles.underline} onPress={handlePressGitHub}>
           source code on GitHub.
         </DoodleText>
+      </DoodleText>
+      <DoodleText style={styles.p} onPress={handlePressInspired}>
+        {'~>'} Inspired by Doodle CSS, Chris McCormick (@mccrmx)
       </DoodleText>
     </View>
   );
