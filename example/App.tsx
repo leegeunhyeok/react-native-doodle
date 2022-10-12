f9cbe2ff51bb29c596db0edaa18e005136f0b388
leegeunhyeok <dev.ghlee@gmail.com>
2022-10-13 03:28:20 +0900

    feat: update demo application
    - add title and some descriptions
    - add DoodleView

--- example/App.tsx
+++ example/App.tsx
@@ -4,10 +4,28 @@ import {
   ScrollView,
   StatusBar,
   StyleSheet,
+  View,
   Text,
   useColorScheme,
-  View,
+  Platform,
+  Linking,
 } from 'react-native';
+import {View as DoodleView} from 'react-native-doodle';
+import type {ComponentProps, PropsWithChildren} from 'react';
+
+const DoodleText = ({
+  children,
+  ...props
+}: PropsWithChildren<ComponentProps<typeof Text>>) => {
+  const isDarkMode = useColorScheme() === 'dark';
+  const textStyle = isDarkMode ? styles.lightText : styles.darkText;
+
+  return (
+    <Text {...props} style={[styles.doodleText, textStyle, props.style]}>
+      {children}
+    </Text>
+  );
+};
 
 const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
@@ -16,19 +34,46 @@ const App = () => {
     backgroundColor: isDarkMode ? '#333' : '#fff',
   };
 
+  const handlePressAuthor = () => {
+    Linking.openURL('https://github.com/leegeunhyeok');
+  };
+
+  const handlePressGitHub = () => {
+    Linking.openURL('https://github.com/leegeunhyeok/react-native-doodle');
+  };
+
   return (
-    <SafeAreaView style={backgroundStyle}>
+    <SafeAreaView style={[styles.flex, backgroundStyle]}>
       <StatusBar
         barStyle={isDarkMode ? 'light-content' : 'dark-content'}
         backgroundColor={backgroundStyle.backgroundColor}
       />
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <View>
-          <Text style={[styles.text, styles.textCenter, styles.h1]}>
-            Doodle
-          </Text>
+        contentContainerStyle={styles.container}>
+        <View style={styles.section}>
+          <DoodleText style={styles.h1}>react-native-doodle</DoodleText>
+          <DoodleText style={styles.p}>
+            A simple hand drawn theme for React Native.
+          </DoodleText>
+          <DoodleText style={styles.p}>
+            By{' '}
+            <DoodleText style={styles.underline} onPress={handlePressAuthor}>
+              Geunhyeok LEE (@leegeunhyeok)
+            </DoodleText>
+          </DoodleText>
+          <DoodleText style={styles.p}>
+            Get the{' '}
+            <DoodleText style={styles.underline} onPress={handlePressGitHub}>
+              source code on GitHub.
+            </DoodleText>
+          </DoodleText>
+        </View>
+        <View style={styles.section}>
+          <DoodleText style={styles.h2}>Quick Demo</DoodleText>
+          <DoodleView style={styles.doodleView}>
+            <DoodleText>wip..</DoodleText>
+          </DoodleView>
         </View>
       </ScrollView>
     </SafeAreaView>
@@ -36,15 +81,45 @@ const App = () => {
 };
 
 const styles = StyleSheet.create({
-  text: {
-    fontFamily: 'Short Stack',
+  flex: {
+    flex: 1,
+  },
+  container: {
+    paddingHorizontal: 16,
+  },
+  doodleText: {
+    fontFamily: Platform.select({
+      ios: 'Short Stack',
+      android: 'ShortStack-Regular',
+    }),
     fontSize: 16,
   },
-  textCenter: {
-    textAlign: 'center',
+  doodleView: {
+    padding: 16,
+  },
+  section: {
+    marginBottom: 24,
+  },
+  underline: {
+    textDecorationLine: 'underline',
+  },
+  lightText: {
+    color: '#e3e3e3',
+  },
+  darkText: {
+    color: '#3c3c3c',
   },
   h1: {
     fontSize: 32,
+    marginVertical: 16,
+  },
+  h2: {
+    fontSize: 28,
+    marginVertical: 16,
+  },
+  p: {
+    fontSize: 16,
+    marginVertical: 8,
   },
 });
 
