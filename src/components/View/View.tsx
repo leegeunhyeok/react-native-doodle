8dd06fe76a37b29e3a432efeb1d319e841395b13
leegeunhyeok <dev.ghlee@gmail.com>
2022-10-14 02:18:37 +0900

    refactor: remove import alias

--- src/components/View/View.tsx
+++ src/components/View/View.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
-import { View as RNView } from 'react-native';
+import { View } from 'react-native';
 import { Rect1 as Border } from '../../assets';
-import { SVG_CONTAINER_STYLE, DEFAULT_SVG_PROPS, DEFAULT_STROKE_COLOR } from '../../constants';
+import { SVG_CONTAINER_STYLE, DEFAULT_SVG_PROPS, DEFAULT_STROKE_COLOR, DEFAULT_STROKE_WIDTH } from '../../constants';
 import type { ComponentProps } from 'react';
 import type { DoodleProps } from '../../types';
 
-type DoodleViewProps = ComponentProps<typeof RNView> & DoodleProps;
+type DoodleViewProps = ComponentProps<typeof View> & DoodleProps;
 
-const DoodleView = ({ strokeColor = DEFAULT_STROKE_COLOR, strokeWidth = DEFAULT_STROKE_COLOR, ...props }: DoodleViewProps) => (
-  <RNView {...props}>
-    <RNView style={SVG_CONTAINER_STYLE}>
+const DoodleView = ({ strokeColor = DEFAULT_STROKE_COLOR, strokeWidth = DEFAULT_STROKE_WIDTH, ...props }: DoodleViewProps) => (
+  <View {...props}>
+    <View style={SVG_CONTAINER_STYLE}>
       <Border {...DEFAULT_SVG_PROPS} strokeColor={strokeColor} strokeWidth={strokeWidth} />
-    </RNView>
+    </View>
     {props.children}
-  </RNView>
+  </View>
 );
 
 export default DoodleView;
